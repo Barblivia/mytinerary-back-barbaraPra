f73b35e3038ced9bb9015bff4a63e112e4322946
Barblivia <barblivia@gmail.com>
2023-08-22 20:33:29 -0300

    Sprint 2 final

--- config/db.js
+++ config/db.js
@@ -5,6 +5,3 @@ mongoose.connect(url_link)
     .then(()=> console.log('Database connected'))
     .catch((err) => console.log(err))
 
-    //let MONGO= 'mongodb+srv://barblivia:bp1234@mytinerary.yh3htvb.mongodb.net/myTinerary'
-    //mongoose.connect(MONGO)
-     //mongoose.connect(process.env.MONGO)
